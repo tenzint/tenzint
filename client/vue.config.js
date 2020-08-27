3bbfe4e2fcb24c946c7a2fca19efaddfb8542ef4
Tenzin Thabkhae <tenzin.thabkhae@mail.utoronto.ca>
2020-08-26 22:11:48 -0400

    vue-cli-plugin-prerender-spa installed

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,4 +2,16 @@ module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
+
+  pluginOptions: {
+    prerenderSpa: {
+      registry: undefined,
+      renderRoutes: [
+        '/',
+      ],
+      useRenderEvent: true,
+      headless: true,
+      onlyProduction: true,
+    },
+  },
 };
